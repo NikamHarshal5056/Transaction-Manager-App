e284ed2ecf633b298bfc865cee65cbb7c02b65a4
Harshal Nikam <nikamharshal5056@gmail.com>
2023-08-26 16:57:25 +0530

    Create JavaApplication1.java

---
+++ JavaApplication1.java
@@ -0,0 +1,17 @@
+// Main Package Class 1
+// Project Name : Transaction Manager, Dekstop App
+// Author : Harshal Nikam
+// Date : 20 Aug 2023
+
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+        
+    }
+    
+}
